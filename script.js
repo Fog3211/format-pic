5b4ddc113eddcaaf444cc6671bf79fa4c0ae1010
fog3211 <384977954@qq.com>
2019-01-25 23:48:12 +0800

    canvas转换图片格式

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+var inputBox = document.getElementById('inputImg'); //上传文件节点
+var imgshow = document.getElementById("imgShow"); //预览节点
+var imgDownload = document.getElementById("download"); //下载按钮节点
+var imgName = '',
+    finish = false;
+
+// 点击转换图片
 document.getElementById('start').addEventListener('click', function () {
     getImg(function (image) {
-        var can = imgToCanvas(image),
-            imgshow = document.getElementById("imgShow");
-        imgshow.setAttribute('src', canvasToImg(can));
+        var can = imgToCanvas(image);
+        imgshow.src = canvasToImg(can);
+        imgShow.style.display = "block";
+        imgDownload.style.cursor = "pointer";
+        finish = true;
     });
 });
+
 // 把image 转换为 canvas对象  
 function imgToCanvas(image) {
     var canvas = document.createElement("canvas");
@@ -13,13 +23,15 @@ function imgToCanvas(image) {
     canvas.getContext("2d").drawImage(image, 0, 0);
     return canvas;
 }
+
 //canvas转换为image
 function canvasToImg(canvas) {
-    var array = ["image/webp", "image/jpg", "image/png"],
+    var array = ["image/webp", "image/jpeg", "image/png"],
         type = document.getElementById('typeSelect').value - 1;
     var src = canvas.toDataURL(array[type]);
     return src;
 }
+
 //获取图片信息
 function getImg(fn) {
     var imgFile = new FileReader();
@@ -28,13 +40,38 @@ function getImg(fn) {
             var image = new Image();
             image.src = this.result; //base64数据 
             image.onload = function () {
-                // var imgSelect=document.getElementById("imgSelect");
-                // imgSelect.setAttribute("src",);
                 fn(image);
             }
         }
-        imgFile.readAsDataURL(document.getElementById('inputImg').files[0]);
+        imgFile.readAsDataURL(inputBox.files[0]);
     } catch (e) {
         alert("请上传图片!");
     }
 }
+
+// 上传图片预览
+inputBox.addEventListener("change", function () {
+    var imgSelect = document.getElementById("imgSelect");
+    var imgUp = document.getElementById("imgUp");
+    var start = document.getElementById("start");
+    imgName = inputBox.files[0].name;
+    var reader = new FileReader();
+    reader.readAsDataURL(inputBox.files[0]);
+    reader.onload = function () {
+        //读取完成后，将结果赋值给img的src
+        imgUp.style.display = "block";
+        imgSelect.src = this.result;
+        start.style.cursor = "pointer";
+    }
+});
+
+// 图片下载
+document.getElementById("download").addEventListener("click", function () {
+    if (finish) {
+        imgDownload.href = imgshow.src;
+        // imgDownload.download = imgshow.src;
+        imgDownload.download = imgName.split(".")[0] + '.' + imgshow.src.split(";")[0].split("/")[1];
+    } else {
+        alert("请上传图片!");
+    }
+});
